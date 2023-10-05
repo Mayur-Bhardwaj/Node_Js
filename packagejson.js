eded428ab5c070490eceeac9500e16ad0664e899
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2023-10-05 15:06:02 +0530

    Info Of Package.Json

---
+++ packagejson.js
@@ -0,0 +1,20 @@
+// Ex:"dependencies": {
+//     "express": "^4.18.2"
+// },
+
+// Version --> " ^ " Means after after that the version will be accepted. If in future the above version is come then  write command npm udate the 18 will be changed into 19.
+//  4 Version Means  "Major Version".
+//  18 Version Means  "Minor Versio".
+//  2 Version Means   "Patch Version".  it is used to fix the bugs. If we write below this version then the things will be alright.
+
+//Ex 2: "dependencies": {
+//     "express": "*4.18.2"
+// },
+
+// " * " Means install anything if the 5 version is also come then update it.
+
+//Ex 3: "dependencies": {
+//     "express": "~4.18.2"
+// },
+
+// " ~ " Means 4.18. will bw fixed it changed then do changes in .2 . When the project is ready then give it.
