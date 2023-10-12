97c7b4fb7f6a1f4ce56d0bfb4f9d37416076795e
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2023-10-12 18:18:57 +0530

    Add files via upload

--- FileSystem/index.js
+++ FileSystem/index.js
@@ -1,2 +1,11 @@
 //Port no. of https is 443.
-const 
+//Make our first web server using node js.
+const http = require('http');
+const { constrainedMemory } = require('process');
+
+//Make a server
+
+const server = http.createServer( (req, res)=>{
+    console.log("Server Started");
+} )
+server.listen(8080);
